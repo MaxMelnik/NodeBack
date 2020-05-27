807be567205973f76800d64f462cae1ef7837546
Maks M <maks.melnik1611@gmail.com>
2020-05-27 17:02:32 +0300

    roomsController: methods for messages implemented

--- source/app.js
+++ source/app.js
@@ -17,39 +17,6 @@ class Application {
         let jsonParser = bodyParser.json();
 
         app.use('/rooms', roomsRouter);
-        app.get('/rooms/:roomId/messages', this.getMessagesHandler.bind(this));
-        app.post('/rooms/:roomId/messages', jsonParser, this.postMessageHandler.bind(this));
-    }
-
-    getMessagesHandler (req, res) {
-        let room = this.manager.getById(req.params.roomId);
-
-        if (!room) {
-            res.status(404).send(`Room with id ${req.params.roomId} doesn't exist`);
-        } else {
-            let messagesJson = room.messages.map(message => message.toJson());
-            let response = {
-                messages: messagesJson
-            };
-            res.json(response);
-        }
-    }
-
-    postMessageHandler (req, res) {
-        let room = this.manager.getById(req.params.roomId);
-
-        if (!room) {
-            res.status(404).send(`Room with id ${req.params.roomId} doesn't exist`);
-        } else if (!req.body.body || !req.body.username) {
-            res.status(400).send('Wrong body format');
-        } else {
-            let message = room.postMessage(req.body.body, req.body.username);
-            let response = {
-                message: message.toJson()
-            };
-
-            res.json(response);
-        }
     }
 
 }
--- source/routes/rooms.js
+++ source/routes/rooms.js
@@ -7,6 +7,8 @@ const RoomsController = require('../controllers/roomsController');
 let jsonParser = bodyParser.json();
 router.get('/', RoomsController.roomSearchHandler.bind(RoomsController));
 router.post('/', jsonParser, RoomsController.createRoomHandler.bind(RoomsController));
+router.get('/:roomId/messages', RoomsController.getMessagesHandler.bind(RoomsController));
+router.post('/:roomId/messages', jsonParser, RoomsController.postMessageHandler.bind(RoomsController));
 
 
 module.exports = router;
